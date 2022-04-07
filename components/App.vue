83bd9d5b7b6e193afcd961648230553b23fae877
bhagath96 <bhagathrudolf@gmail.com>
2022-04-07 14:20:34 +0530

    remove shadow

--- components/App.vue
+++ components/App.vue
@@ -4,10 +4,10 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
         </div>
         <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8" >
-          <div v-if="this.$route.path ==='/Dashboard'" class="py-4" style="box-shadow:0px 0px 8px 0px #888888;">
+          <div v-if="this.$route.path ==='/Dashboard'" class="py-4" style="box-shadow:0px 0px 6px 0px #e4e8eb">
            <Table/>
           </div>
-          <div v-if="this.$route.path ==='/Employee/Addemployee'" class="py-4" style="box-shadow:0px 0px 8px 0px #888888;">
+          <div v-if="this.$route.path ==='/Employee/Addemployee'" class="py-4" style="box-shadow:0px 0px 6px 0px #e4e8eb">
            <Tabview/>
           </div>
 
